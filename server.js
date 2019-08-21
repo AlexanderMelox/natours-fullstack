7f3d73fcd860ab6a5b1aab3fddb149ae640f34d0
Alexander Melo <alexander.melo@ibm.com>
2019-08-21 13:45:51 -0500

    added eslint-prettier

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
 // Import the express app
@@ -7,5 +8,5 @@ const app = require('./app');
 // Specifies the port for the app to listen to
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App running on port http://localhost:${port}`);
+  console.log(`App running on port http://localhost:${port}`); //eslint-disable-line
 });
